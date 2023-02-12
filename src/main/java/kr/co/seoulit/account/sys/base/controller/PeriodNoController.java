98a8d1b2baa7f3c475f7f94756957ed09b525fdb
bobby <akirasahy@gmail.com>
2023-02-12 22:31:27 +0900

    peridNo 수정중

--- src/main/java/kr/co/seoulit/account/sys/base/controller/PeriodNoController.java
+++ src/main/java/kr/co/seoulit/account/sys/base/controller/PeriodNoController.java
@@ -1,10 +1,13 @@
 package kr.co.seoulit.account.sys.base.controller;
 
-
+import kr.co.seoulit.account.posting.business.to.SlipBean;
 import kr.co.seoulit.account.sys.base.service.BaseService;
+import kr.co.seoulit.account.sys.base.to.PeriodNoBean;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @CrossOrigin("*")
@@ -12,13 +15,23 @@ import java.util.HashMap;
 @RequestMapping("/settlement")
 public class PeriodNoController {
 
-    @Autowired
-    private BaseService baseService;
+	@Autowired
+	private BaseService baseService;
 
-    @GetMapping("/periodNoList")
-    public HashMap<String, Object> findPeriodNo(){
-        HashMap<String , Object> map = new HashMap<>();
-        map.put("periodNoList" , baseService.findPeriodNo());
-        return map;
-    }
+	@GetMapping("/periodNoList")
+	public HashMap<String, Object> findPeriodNo() {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("periodNoList", baseService.findPeriodNo());
+		return map;
+	}
+
+	@GetMapping("/tPeriodNoList")
+	public ArrayList<PeriodNoBean> findPeriodNo(@RequestParam("yearFirst") String yearFirst,
+			@RequestParam("yearLast") String yearLast) {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("yearFirst", yearFirst);
+		map.put("yearLast", yearLast);
+		ArrayList<PeriodNoBean> PeriodNoBean = baseService.findTPeriodNo(map);
+		return PeriodNoBean;
+	}
 }
