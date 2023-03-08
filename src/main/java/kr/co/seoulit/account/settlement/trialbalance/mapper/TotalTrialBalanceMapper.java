a867aa4cfb1746440edbd682de99de719378ac4c
bobbybeatts <akirasahy@gmail.com>
2023-03-09 08:59:00 +0900

    0308 저녁 커밋-인데 09일날 커밋함

--- src/main/java/kr/co/seoulit/account/settlement/trialbalance/mapper/TotalTrialBalanceMapper.java
+++ src/main/java/kr/co/seoulit/account/settlement/trialbalance/mapper/TotalTrialBalanceMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import kr.co.seoulit.account.settlement.trialbalance.to.DetailTrialBalanceBean;
 import kr.co.seoulit.account.settlement.trialbalance.to.EarlyAssetBean;
+import kr.co.seoulit.account.settlement.trialbalance.to.TotalTrialBalanceBean;
 
 @Mapper
 public interface TotalTrialBalanceMapper {
@@ -18,4 +19,8 @@ public interface TotalTrialBalanceMapper {
     public ArrayList<DetailTrialBalanceBean> selectDetailTrialBalance(HashMap<String, Object> params);
 
 	public HashMap<String, Object> selectAccountingSettlement(HashMap<String, String> map);
+	
+//	public HashMap<String, Object> selectTotalTrialBalance(HashMap<String,Object> map);
+	
+	public ArrayList<TotalTrialBalanceBean> selectTotalTrialBalance(HashMap<String, Object> map);
 }
